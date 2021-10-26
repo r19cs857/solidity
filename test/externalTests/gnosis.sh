e0542f45ff2ebef5f91abf6924f5d506f6f0edec
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-10-26 11:12:45 +0200

    Update gnosis external test to use upstream directly and use Hardhat

--- test/externalTests/gnosis.sh
+++ test/externalTests/gnosis.sh
@@ -28,23 +28,25 @@ verify_input "$@"
 BINARY_TYPE="$1"
 BINARY_PATH="$2"
 
-function compile_fn { npx truffle compile; }
+function compile_fn { npm run build; }
 function test_fn { npm test; }
 
 function gnosis_safe_test
 {
-    local repo="https://github.com/solidity-external-tests/safe-contracts.git"
-    local branch=development_080
-    local config_file="truffle-config.js"
+    local repo="https://github.com/gnosis/safe-contracts.git"
+    local branch=main
+    local config_file="hardhat.config.ts"
+    local config_var=userConfig
 
-    local compile_only_presets=()
+    local compile_only_presets=(
+        ir-optimize-evm+yul        # Compiles but tests fail. See https://github.com/nomiclabs/hardhat/issues/2115
+    )
     local settings_presets=(
         "${compile_only_presets[@]}"
-        #ir-no-optimize            # "YulException: Variable var_call_430_mpos is 1 slot(s) too deep inside the stack."
-        #ir-optimize-evm-only      # "YulException: Variable var_call_430_mpos is 1 slot(s) too deep inside the stack."
-        ir-optimize-evm+yul
-        #legacy-no-optimize        # "Stack too deep" error
-        #legacy-optimize-evm-only  # "Stack too deep" error
+        #ir-no-optimize            # Compilation fails with "YulException: Variable var_call_430_mpos is 1 slot(s) too deep inside the stack."
+        #ir-optimize-evm-only      # Compilation fails with "YulException: Variable var_module_1480 is 9 slot(s) too deep inside the stack."
+        legacy-no-optimize
+        legacy-optimize-evm-only
         legacy-optimize-evm+yul
     )
 
@@ -56,18 +58,28 @@ function gnosis_safe_test
     download_project "$repo" "$branch" "$DIR"
     [[ $BINARY_TYPE == native ]] && replace_global_solc "$BINARY_PATH"
 
-    sed -i 's|github:gnosis/mock-contract#sol_0_5_0|github:solidity-external-tests/mock-contract#master_080|g' package.json
+    # NOTE: The patterns below intentionally have hard-coded versions.
+    # When the upstream updates them, there's a chance we can just remove the regex.
+    sed -i 's|"@gnosis\.pm/mock-contract": "\^4\.0\.0"|"@gnosis.pm/mock-contract": "github:solidity-external-tests/mock-contract#master_080"|g' package.json
+    sed -i 's|"@openzeppelin/contracts": "\^3\.4\.0"|"@openzeppelin/contracts": "^4.0.0"|g' package.json
+
+    # Disable two tests failing due to Hardhat's heuristics not yet updated to handle solc 0.8.10.
+    # TODO: Remove this when Hardhat implements them (https://github.com/nomiclabs/hardhat/issues/2051).
+    sed -i "s|\(it\)\(('should revert if called directly', async () => {\)|\1.skip\2|g" test/handlers/CompatibilityFallbackHandler.spec.ts
 
     neutralize_package_lock
     neutralize_package_json_hooks
-    force_truffle_compiler_settings "$config_file" "$BINARY_TYPE" "${DIR}/solc" "$(first_word "$selected_optimizer_presets")"
-    npm install --package-lock
+    force_hardhat_compiler_binary "$config_file" "$BINARY_TYPE" "$BINARY_PATH"
+    force_hardhat_compiler_settings "$config_file" "$(first_word "$selected_optimizer_presets")" "$config_var"
+    npm install
 
     replace_version_pragmas
     [[ $BINARY_TYPE == solcjs ]] && force_solc_modules "${DIR}/solc"
 
+    replace_version_pragmas
+
     for preset in $selected_optimizer_presets; do
-        truffle_run_test "$config_file" "$BINARY_TYPE" "${DIR}/solc" "$preset" "${compile_only_presets[*]}" compile_fn test_fn
+        hardhat_run_test "$config_file" "$preset" "${compile_only_presets[*]}" compile_fn test_fn "$config_var"
     done
 }
 
