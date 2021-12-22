6a1e67f636275cd62d011a1bdbc81f7f4a64ea7d
chriseth <chris@ethereum.org>
2021-12-22 15:38:50 +0100

    Try to fix windows.

--- .circleci/soltest.ps1
+++ .circleci/soltest.ps1
@@ -5,22 +5,16 @@ cd "$PSScriptRoot\.."
 .\build\solc\Release\solc.exe --version
 if ( -not $? ) { throw "Cannot execute solc --version." }
 
+# How to report errors?
 mkdir test_results
-{
-    .\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result.xml -- --no-smt --batches 2 --selected-batch 0
-    if ( -not $? ) { throw "Unoptimized soltest run 0 failed." }
-} &
-{
-    .\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result.xml -- --no-smt --batches 2 --selected-batch 1
-    if ( -not $? ) { throw "Unoptimized soltest run 1 failed." }
-} &
-{
-    .\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result_opt.xml -- --optimize --no-smt --batches 2 --selected-batch 0
-    if ( -not $? ) { throw "Optimized soltest run 0 failed." }
-} &
-{
-    .\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result_opt.xml -- --optimize --no-smt --batches 2 --selected-batch 1
-    if ( -not $? ) { throw "Optimized soltest run 1 failed." }
-} &
+.\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result.xml -- --no-smt --batches 2 --selected-batch 0 &
+#    if ( -not $? ) { throw "Unoptimized soltest run 0 failed." }
+.\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result.xml -- --no-smt --batches 2 --selected-batch 1 &
+#    if ( -not $? ) { throw "Unoptimized soltest run 1 failed." }
+.\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result_opt.xml -- --optimize --no-smt --batches 2 --selected-batch 0 &
+#    if ( -not $? ) { throw "Optimized soltest run 0 failed." }
+.\build\test\Release\soltest.exe --color_output=no --show_progress=yes --logger=JUNIT,error,test_results/result_opt.xml -- --optimize --no-smt --batches 2 --selected-batch 1 &
+#    if ( -not $? ) { throw "Optimized soltest run 1 failed." }
 
 Get-Job | Wait-Job
+if ( -not $? ) { throw "tes run failed." }
